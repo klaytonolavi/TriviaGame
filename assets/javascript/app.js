5773b4f70b6c5187214dcc1c60783563ce73b7c7
Klayton <kliekkio@gmail.com>
2017-08-22 19:47:02 -0700

    third commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// make array that contains questions and answers
+
+// give correct answers to each question
+
+// create a timer that starts 5 seconds after the page loads
+
+// make the time remaining count down for 1 minute
+
+
+// when the timer remaining reaches 0 then
+// create alert that shows the amount of questions
+// answered correctly and incorrectly.
+
